cc65ce1814d91da2592d0583201a307d7e361311
jj88 <juanjosepm@live.com>
2017-06-15 13:33:51 -0500

    .

--- javascript/new/student.js
+++ javascript/new/student.js
@@ -23,11 +23,11 @@ function desactivar(id,activo){
 			console.log(response)
 			var splitResp = response.split("[#]");
 			
-		// DoSearch();
+		DoSearch();
 			if(splitResp[0] == "ok"){
 					// $("#tblContent").html(splitResp[2]);
 					// alert("d");
-					DoSearch();
+					// DoSearch();
 				}
 			else if(splitResp[0] == "fail"){
 				$("#res_").html(splitResp[1]);
@@ -58,10 +58,10 @@ function activar(id,activo){
 			console.log(response)
 			var splitResp = response.split("[#]");
 			
-		
+		DoSearch()
 			if(splitResp[0] == "ok"){
 					// $("#tblContent").html(splitResp[2]);
-					DoSearch()
+					
 				}
 			else if(splitResp[0] == "fail"){
 				$("#res_").html(splitResp[1]);
