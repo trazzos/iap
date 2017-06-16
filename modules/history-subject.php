7b7f138ce36ed9d5c1bf9c3a878566f497e7bdc8
jj88 <juanjosepm@live.com>
2017-06-16 13:31:20 -0500

    bug: paginador

--- modules/history-subject.php
+++ modules/history-subject.php
@@ -15,7 +15,7 @@
 	// -------------------------------------------------------------------------------------------------
 	$arrPage = array();		// ---- arreglo donde guarda los resultados de la paginacion...para usarse en footer-pages-links.tpl
 	$viewPage = 1;			// ---- por default se toma la primera pagina, por si aun no esta definidala en la variable GET
-	$rowsPerPage = 100000000000;		//<<--- se podria tomar este valor de una variable o constante global, o especificarla para un caso particular
+	$rowsPerPage = 500;		//<<--- se podria tomar este valor de una variable o constante global, o especificarla para un caso particular
 	$pageVar = 'p';	// ---- el nombre de la variable en GET que trae la pagina a mostrar, en este caso se usa viewPage para pasar la pagina a visualizar
 	if(isset($_GET["$pageVar"]))
 		$viewPage = $_GET["$pageVar"];	//si ya esta definida la variable GET['viewPage'] tomar el valor de esta
@@ -25,19 +25,19 @@
 	$result = $course->EnumerateByPage($viewPage, $rowsPerPage, $pageVar, WEB_ROOT.'/history-subject', $arrPage);	
 	
 	//checar a que curriculas tengo permiso
-	// if(in_array(2, $info["roles"]))
-	// {
-		// $smarty->assign('docente', 1);
-		// $permisosDocente = $user->PermisosDocente();
+	if(in_array(2, $info["roles"]))
+	{
+		$smarty->assign('docente', 1);
+		$permisosDocente = $user->PermisosDocente();
 		
-		// foreach($result as $key => $value)
-		// {
-			// if(!in_array($value["courseId"], $permisosDocente["course"]))
-			// {
-				// unset($result[$key]);
-			// }
-		// }
-	// }
+		foreach($result as $key => $value)
+		{
+			if(!in_array($value["courseId"], $permisosDocente["course"]))
+			{
+				unset($result[$key]);
+			}
+		}
+	}
 	
 	$smarty->assign('subjects', $result);
 	$smarty->assign('arrPage', $arrPage);
