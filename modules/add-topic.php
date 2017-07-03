d48883cd42c79fe203220755d7717e5c6006e5a0
jj88 <juanjosepm@live.com>
2017-07-03 07:45:20 -0500

    coregir bug en foros

--- modules/add-topic.php
+++ modules/add-topic.php
@@ -2,6 +2,10 @@
 		
 	/* For Session Control - Don't remove this */
 	//$user->allow_access(8);	
+	
+	// echo "<pre>"; print_r($_POST);
+	// echo "<pre>"; print_r($_GET);
+	// exit;
 $forum->setTopicId($_GET["id"]);
 /*print_r($_SESSION["User"]["userId"]);exit;*/
 
@@ -16,7 +20,7 @@ $forum->setTopicId($_GET["id"]);
 	
 		// echo "<pre>"; print_r($_POST);
 		// exit;
-		header("Location:".WEB_ROOT."/forumsub-modules-student/id/12/topicId/".$_POST["topicId"]."");
+		header("Location:".WEB_ROOT."/forumsub-modules-student/id/".$_GET["cId"]."/topicId/".$_POST["topicId"]."");
 		exit;
 	}
 	
