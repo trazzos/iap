aed828451b4f27fcac5b41666c315d3c1c4eeacd
root <root@centos-iap.iapchiapas.org.mx>
2018-07-24 18:41:16 +0000

    varios cambios

--- ajax/calendario.php
+++ ajax/calendario.php
@@ -5,6 +5,10 @@
 	include_once('../config.php');
 	include_once(DOC_ROOT.'/libraries.php');
 
+use Dompdf\Adapter\CPDF;
+use Dompdf\Dompdf;
+use Dompdf\Exception;
+
 	session_start();
 
 	
@@ -101,7 +105,7 @@
 	$mipdf ->set_paper("A4", "landscape");
 	 
 	# Cargamos el contenido HTML.
-	$mipdf ->load_html(utf8_decode($html));
+	$mipdf ->load_html($html);
 	 
 	# Renderizamos el documento PDF.
 	$mipdf ->render();
--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php 
-    ini_set("display_errors", 1); 
+    ini_set("display_errors", 0); 
     $showErrors = E_ALL ^ (E_STRICT | E_NOTICE | E_DEPRECATED);
     //$showErrors = E_ALL;
-    error_reporting($showErrors);
+    error_reporting(0);
 	
 	if(function_exists('xdebug_disable'))
 		xdebug_disable();
@@ -18,24 +18,4 @@
 		mb_regex_encoding('UTF-8');
 		ob_start('mb_output_handler');
 	}
-// ini_set("display_errors", "ON"); 
-// switch($version)
-// {
- // case "5.3": $showErrors = E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT; break;
- // case "5.4": $showErrors = E_ALL ^ (E_STRICT | E_NOTICE | E_DEPRECATED); break;
- // default: $showErrors = E_ALL ^ (E_STRICT | E_NOTICE | E_DEPRECATED); break;
-// }
-// error_reporting($showErrors);
-
-// date_default_timezone_set('America/Mexico_City');
-
-// header('Content-type: text/html; charset=utf-8');
-
-// mb_internal_encoding('UTF-8');
-// mb_http_output('UTF-8');
-// mb_http_input('UTF-8');
-// mb_language('uni');
-// mb_regex_encoding('UTF-8');
-
-// ob_start('mb_output_handler');
 ?>
